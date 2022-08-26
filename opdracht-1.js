32604cbafc166d9af9993176542b24efb512f193
MikaJ010 <majongma@outlook.com>
2022-08-26 12:56:41 +0200

    Opdracht 1 af

---
+++ opdracht-1.js
@@ -0,0 +1,15 @@
+// Opdracht 1: Array methoden ---------------
+
+
+// Opdracht 1A:
+// Stap 1: Maak een variable aan genaamd typeName waar de output in opgevangen wordt
+// Stap 2: Schrijf een .map() methode waarin op type wordt gefilterd
+// Stap 3: Vergeet de return niet
+// Stap 4: Log de uitkomst in de console
+
+const typeNumber = inventory.map((tv) => {
+    return tv.type;
+    }
+)
+
+console.log(typeNumber);
